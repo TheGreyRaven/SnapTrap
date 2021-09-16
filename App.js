0ab022ea472277a5dbbf3a418c04bc1eaf633aaa
TheGreyRaven <theravengrey@gmail.com>
2021-09-16 16:55:19 +0200

    Added Matomo app download tracking.

--- App.js
+++ App.js
@@ -6,15 +6,20 @@ import { Header } from './components/Header';
 import { TabNavigator } from './components/BottomNav.js';
 import { default as theme } from './custom-theme.json';
 import { NavigationContainer } from '@react-navigation/native';
+import Matomo from 'react-native-matomo';
 
-export default () => (
-	<>
-		<IconRegistry icons={EvaIconsPack} />
-		<ApplicationProvider {...eva} theme={{ ...eva.light, ...theme }} style={{ backgroundColor: '#FBFBFB' }}>
-			<Header />
-			<NavigationContainer>
-				<TabNavigator />
-			</NavigationContainer>
-		</ApplicationProvider>
-	</>
-);
+export default () => {
+	Matomo.initTracker('https://analytics.snaptrap.io/piwik.php', 1);
+	Matomo.trackAppDownload();
+	return (
+		<>
+			<IconRegistry icons={EvaIconsPack} />
+			<ApplicationProvider {...eva} theme={{ ...eva.light, ...theme }} style={{ backgroundColor: '#FBFBFB' }}>
+				<Header />
+				<NavigationContainer>
+					<TabNavigator />
+				</NavigationContainer>
+			</ApplicationProvider>
+		</>
+	)
+};
