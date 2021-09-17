62fb33223b794f34e517d0fb23f9a8f58aca0003
TheGreyRaven <theravengrey@gmail.com>
2021-09-17 13:43:36 +0200

    Added missing file extension

--- App.js
+++ App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { ApplicationProvider, IconRegistry } from '@ui-kitten/components';
 import * as eva from '@eva-design/eva';
 import { EvaIconsPack } from '@ui-kitten/eva-icons';
-import { Header } from './components/Header';
+import { Header } from './components/Header.js';
 import { TabNavigator } from './components/BottomNav.js';
 import { default as theme } from './custom-theme.json';
 import { NavigationContainer } from '@react-navigation/native';
