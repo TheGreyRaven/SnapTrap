ba94ff61a472b35af205fd2684de144282894dc2
TheGreyRaven <theravengrey@gmail.com>
2021-09-17 14:48:05 +0200

    Remove matomo

--- android/app/src/main/java/com/snaptrap/MainApplication.java
+++ android/app/src/main/java/com/snaptrap/MainApplication.java
@@ -10,7 +10,6 @@ import com.facebook.react.ReactPackage;
 import com.facebook.soloader.SoLoader;
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
-import de.bonify.reactnativematomo.MatomoPackage;
 
 public class MainApplication extends Application implements ReactApplication {
 
